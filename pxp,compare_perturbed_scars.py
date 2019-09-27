1cffb2e1c2e54dd3c8202a32b422fac8840167cc
Kieran Bull <pykb@localhost.localdomain>
2019-09-27 01:04:33 +0100

    added graph of PPXP eigenstates, highlighting those with largest overlap with PXXXP scars

--- pxp,compare_perturbed_scars.py
+++ pxp,compare_perturbed_scars.py
@@ -27,7 +27,7 @@ rc('font',**{'family':'sans-serif','sans-serif':['Computer Modern'],'size':26})
 rc('text', usetex=True)
 # matplotlib.rcParams['figure.dpi'] = 400
 
-N = 20
+N = 14
 pxp = unlocking_System([0],"periodic",2,N)
 pxp.gen_basis()
 pxp_syms = model_sym_data(pxp,[translational(pxp),parity(pxp)])
@@ -76,9 +76,41 @@ M = np.zeros((np.size(scar_indices_ppxp),np.size(scar_indices_pxxxp)),dtype=comp
 for n in range(0,np.size(M,axis=0)):
     for m in range(0,np.size(M,axis=0)):
         M[n,m] = np.vdot(H1.sector.eigvectors()[:,scar_indices_ppxp[n]],H2.sector.eigvectors()[:,scar_indices_pxxxp[m]])
+print(np.diag(M))
 plt.matshow(np.abs(M))
 plt.colorbar()
 plt.show()
+
+ppxp_eig_largest_pxxxp_scar_overlap = np.zeros(np.size(scar_indices_pxxxp))
+ppxp_eig_largest_pxxxp_scar_indices = np.zeros(np.size(scar_indices_pxxxp))
+scar_e = np.zeros(np.size(scar_indices_pxxxp))
+scar_o = np.zeros(np.size(scar_indices_pxxxp))
+max_overlaps = np.zeros(np.size(scar_indices_pxxxp))
+for n in np.arange(0,np.size(scar_indices_pxxxp,axis=0)):
+    max_overlap = 0
+    max_index = None
+    for m in np.arange(0,np.size(H1.sector.eigvectors(),axis=1)):
+        temp = np.abs(np.vdot(H1.sector.eigvectors()[:,m],H2.sector.eigvectors()[:,scar_indices_pxxxp[n]]))**2
+        if temp > max_overlap:
+            max_overlap = temp
+            max_index = m
+    ppxp_eig_largest_pxxxp_scar_overlap[n] = max_overlap
+    ppxp_eig_largest_pxxxp_scar_indices[n] = max_index
+    scar_e[n] = H1.sector.eigvalues()[max_index]
+    scar_o[n] = overlap1[max_index]
+plt.scatter(H1.sector.eigvalues(),overlap1)
+plt.scatter(scar_e,scar_o,marker="D",color="red",s=100,alpha=0.6,label="PPXP eigenstates with largest overlap with PXXXP scars")
+print(scar_e)
+print(scar_o)
+print(ppxp_eig_largest_pxxxp_scar_overlap)
+plt.xlabel(r"$E$")
+plt.ylabel(r"$\vert \langle Z_2 \vert E \rangle \vert^2$")
+plt.legend()
+plt.show()
+    
+
+        
+    
     
     
 
