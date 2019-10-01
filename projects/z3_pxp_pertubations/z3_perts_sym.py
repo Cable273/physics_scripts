41132b08fccc5d0d23ee2ed32312c697904cfa01
Kieran Bull <pykb@localhost.localdomain>
2019-10-01 23:07:56 +0100

    updated syntax

--- projects/z3_pxp_pertubations/z3_perts_sym.py
+++ projects/z3_pxp_pertubations/z3_perts_sym.py
@@ -20,108 +20,104 @@ from rw_functions import save_obj,load_obj
 from Calculations import level_stats,fidelity,eig_overlap,entropy,site_precession,site_projection,time_evolve_state
 
 from matplotlib import rc
-rc('font',**{'family':'sans-serif','sans-serif':['Computer Modern'],'size':26})
+rc('font',**{'family':'sans-serif','sans-serif':['Computer Modern'],'size':36})
 ## for Palatino and other serif fonts use:
 #rc('font',**{'family':'serif','serif':['Palatino']})
 rc('text', usetex=True)
 # matplotlib.rcParams['figure.dpi'] = 400
 
-N = 12
+N = 9
 pxp = unlocking_System([0],"periodic",2,N)
 pxp.gen_basis()
-pxp_syms=model_sym_data(pxp,[translational_general(pxp,order=3)])
+pxp_syms=model_sym_data(pxp,[translational_general(pxp,order=3),PT(pxp)])
+    
 z=zm_state(3,1,pxp)
 k=pxp_syms.find_k_ref(z.ref)
-print(k)
 
-V1_ops = dict()
-V1_ops[0] = Hamiltonian(pxp,pxp_syms)
-V1_ops[0].site_ops[1] = np.array([[0,1],[1,0]])
-V1_ops[0].model = np.array([[0,1,0,0]])
-V1_ops[0].model_coef = np.array([1])
+V1 = Hamiltonian(pxp,pxp_syms)
+V1.site_ops[1] = np.array([[0,1],[1,0]])
+V1.model = np.array([[0,1,0,0],[0,0,1,0],[0,1,0,0],[0,0,1,0]])
+V1.model_coef = np.array([1,1,1,1])
+V1.uc_size = np.array([3,3,3,3])
+V1.uc_pos = np.array([1,2,2,1])
 for n in range(0,np.size(k,axis=0)):
-    V1_ops[0].gen(k_vec=k[n],uc_size=3,uc_pos=1)
+    V1.gen(k_vec=k[n])
 
-V1_ops[1] = Hamiltonian(pxp,pxp_syms)
-V1_ops[1].site_ops[1] = np.array([[0,1],[1,0]])
-V1_ops[1].model = np.array([[0,0,1,0]])
-V1_ops[1].model_coef = np.array([1])
+V2 = Hamiltonian(pxp,pxp_syms)
+V2.site_ops[1] = np.array([[0,1],[1,0]])
+V2.model = np.array([[0,0,1,0],[0,1,0,0]])
+V2.model_coef = np.array([1,1])
+V2.uc_size = np.array([3,3])
+V2.uc_pos = np.array([0,0])
 for n in range(0,np.size(k,axis=0)):
-    V1_ops[1].gen(k_vec=k[n],uc_size=3,uc_pos=2)
+    V2.gen(k_vec=k[n])
 
-V1_ops[2] = Hamiltonian(pxp,pxp_syms)
-V1_ops[2].site_ops[1] = np.array([[0,1],[1,0]])
-V1_ops[2].model = np.array([[0,1,0,0]])
-V1_ops[2].model_coef = np.array([1])
+V3 = Hamiltonian(pxp,pxp_syms)
+V3.site_ops[1] = np.array([[0,1],[1,0]])
+V3.model = np.array([[0,1,1,1,0],[0,1,1,1,0]])
+V3.model_coef = np.array([1,1])
+V3.uc_size = np.array([3,3])
+V3.uc_pos = np.array([0,2])
 for n in range(0,np.size(k,axis=0)):
-    V1_ops[2].gen(k_vec=k[n],uc_size=3,uc_pos=2)
-
-V1_ops[3] = Hamiltonian(pxp,pxp_syms)
-V1_ops[3].site_ops[1] = np.array([[0,1],[1,0]])
-V1_ops[3].model = np.array([[0,0,1,0]])
-V1_ops[3].model_coef = np.array([1])
-for n in range(0,np.size(k,axis=0)):
-    V1_ops[3].gen(k_vec=k[n],uc_size=3,uc_pos=1)
-
-V1 = V1_ops[0]
-for n in range(1,len(V1_ops)):
-    V1=H_operations.add(V1,V1_ops[n],np.array([1,1]))
-
-V2_ops = dict()
-V2_ops[0] = Hamiltonian(pxp,pxp_syms)
-V2_ops[0].site_ops[1] = np.array([[0,1],[1,0]])
-V2_ops[0].model = np.array([[0,0,1,0]])
-V2_ops[0].model_coef = np.array([1])
-for n in range(0,np.size(k,axis=0)):
-    V2_ops[0].gen(k_vec=k[n],uc_size=3,uc_pos=0)
-
-V2_ops[1] = Hamiltonian(pxp,pxp_syms)
-V2_ops[1].site_ops[1] = np.array([[0,1],[1,0]])
-V2_ops[1].model = np.array([[0,1,0,0]])
-V2_ops[1].model_coef = np.array([1])
-for n in range(0,np.size(k,axis=0)):
-    V2_ops[1].gen(k_vec=k[n],uc_size=3,uc_pos=0)
-
-V2 = V2_ops[0]
-for n in range(1,len(V2_ops)):
-    V2=H_operations.add(V2,V2_ops[n],np.array([1,1]))
-
-V3_ops = dict()
-V3_ops[0] = Hamiltonian(pxp,pxp_syms)
-V3_ops[0].site_ops[1] = np.array([[0,1],[1,0]])
-V3_ops[0].model = np.array([[0,1,1,1,0]])
-V3_ops[0].model_coef = np.array([1])
-for n in range(0,np.size(k,axis=0)):
-    V3_ops[0].gen(k_vec=k[n],uc_size=3,uc_pos=0)
-
-V3_ops[1] = Hamiltonian(pxp,pxp_syms)
-V3_ops[1].site_ops[1] = np.array([[0,1],[1,0]])
-V3_ops[1].model = np.array([[0,1,1,1,0]])
-V3_ops[1].model_coef = np.array([1])
-for n in range(0,np.size(k,axis=0)):
-    V3_ops[1].gen(k_vec=k[n],uc_size=3,uc_pos=2)
-
-V3 = V3_ops[0]
-for n in range(1,len(V3_ops)):
-    V3=H_operations.add(V3,V3_ops[n],np.array([1,1]))
+    V3.gen(k_vec=k[n])
 
 H0 = spin_Hamiltonian(pxp,"x",pxp_syms)
 for n in range(0,np.size(k,axis=0)):
     H0.gen(k[n])
+
+V1.gen()
+V2.gen()
+V3.gen()
+H0.gen()
 
 coef = np.array([0.18243653,-0.10390499,0.054452])
 H = H_operations.add(H0,V1,np.array([1,coef[0]]))
 H = H_operations.add(H,V2,np.array([1,coef[1]]))
 H = H_operations.add(H,V3,np.array([1,coef[2]]))
 
+coef = np.array([0.09992653,-0.17003587,0.01092036])
+H1 = H_operations.add(H0,V1,np.array([1,coef[0]]))
+H1 = H_operations.add(H1,V2,np.array([1,coef[1]]))
+H1 = H_operations.add(H1,V3,np.array([1,coef[2]]))
+
+coef = np.array([0.10456971,-0.16439141,0.03094977])
+H2 = H_operations.add(H0,V1,np.array([1,coef[0]]))
+H2 = H_operations.add(H2,V2,np.array([1,coef[1]]))
+H2 = H_operations.add(H2,V3,np.array([1,coef[2]]))
+
+# coef = np.array([0.14642557,-0.16027117,0.0218123])
+coef = np.array([0.1494363,-0.16027541,0.02084993])
+H3 = H_operations.add(H0,V1,np.array([1,coef[0]]))
+H3 = H_operations.add(H3,V2,np.array([1,coef[1]]))
+H3 = H_operations.add(H3,V3,np.array([1,coef[2]]))
+
 for n in range(0,np.size(k,axis=0)):
     H.sector.find_eig(k[n])
-    print(H.sector.eigvalues(k[n]))
-    overlap = eig_overlap(z,H,k[n]).eval()
-    plt.scatter(H.sector.eigvalues(k[n]),overlap,color="blue")
-    # eig_overlap(z,H,k[n]).plot()
-plt.show()
+    H1.sector.find_eig(k[n])
+    H2.sector.find_eig(k[n])
+    H3.sector.find_eig(k[n])
+H.sector.find_eig()
+print(H.sector.eigvalues())
+print(H.sector.eigvalues(k[0]))
 
-fidelity(z,H,"use sym").plot(np.arange(0,20,0.01),z)
-plt.show()
+# t=np.arange(0,20,0.01)
+# f = fidelity(z,H,"use sym").eval(t,z)
+# f1 = fidelity(z,H1,"use sym").eval(t,z)
+# f2 = fidelity(z,H2,"use sym").eval(t,z)
+# f3 = fidelity(z,H3,"use sym").eval(t,z)
+# np.save("pxp,z3_perts,t,"+str(pxp.N),t)
+# np.save("pxp,z3_perts,f,f0,"+str(pxp.N),f)
+# np.save("pxp,z3_perts,f,lie_algebra,"+str(pxp.N),f1)
+# np.save("pxp,z3_perts,f,hz_var,"+str(pxp.N),f2)
+# np.save("pxp,z3_perts,f,subspace_var,"+str(pxp.N),f3)
+
+# plt.plot(t,f,linewidth=2,label=r"$f_0$")
+# plt.plot(t,f1,linewidth=2,alpha=1.0,label=r"Lie algebra")
+# plt.plot(t,f2,linewidth=2,alpha=1.0,label=r"$H^z$ Variance")
+# plt.plot(t,f3,linewidth=2,alpha=1.0,label=r"Subspace Variance")
+# plt.legend()
+# plt.xlabel(r"$t$")
+# plt.ylabel(r"$\vert \langle \psi(0) \vert \psi(t) \rangle \vert^2$")
+# plt.title(r"$PXP + \lambda_i V+i$, $Z_3$ pertubations, $N=$"+str(pxp.N))
+# plt.show()
 
